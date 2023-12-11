490f908a9b40f5be5694406fc0dd7158d776767d
kurtskhala <92853046+kurtskhala@users.noreply.github.com>
2023-12-12 01:37:24 +0400

    add login and rating

--- src/App.js
+++ src/App.js
@@ -6,21 +6,28 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Movie from './components/Movie';
 import NotFound from './components/NotFound';
+import Home from './components/Home';
+import Login from './components/Login';
+
+//Context
+import UserProvider from './context';
 
 //Styles
 import { GlobalStyle } from './GlobalStyle';
-import Home from './components/Home';
 
 const App = () => (
-    <Router>
+  <Router>
+    <UserProvider>
       <Header />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/:movieId' element={<Movie />} />
-        <Route path='/*' element={<NotFound />} />
+        <Route path="/" element={<Home />} />
+        <Route path='/login' element={<Login/>} />
+        <Route path="/:movieId" element={<Movie />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
       <GlobalStyle />
-    </Router>
+    </UserProvider>
+  </Router>
 );
 
 export default App;
